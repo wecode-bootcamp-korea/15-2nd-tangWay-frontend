9bd7cad5e3cbbed5688c33354ea62fa39e756856
zoo0190 <66614341+zoo0190@users.noreply.github.com>
2020-12-28 19:54:46 +0900

    Add: 초기셋팅

---
+++ src/Pages/Booking/Components/BookingHeader.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BookingHeader() {
+  return <div></div>;
+}
+
+export default BookingHeader;
---
+++ src/Routes.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Main from "./Pages/Main/Main";
+import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/Signup";
+import Booking from "./Pages/Booking/Booking";
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/Booking" component={Booking} />
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/Signup" component={Signup} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+export default Routes;
