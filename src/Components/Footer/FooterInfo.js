fbbf242b164254632318c169a469a60600ec3803
강경오 <gang-gyeong-o@gang-gyeong-oui-MacBookPro.local>
2021-01-07 16:36:39 +0900

    Fix: 리뷰 반영 수정 완료

---
+++ src/Components/Footer/FooterInfo.js
@@ -0,0 +1,26 @@
+export const twayInfo = {
+  info1: [
+    {
+      title: "Team TangWay 소개1",
+      front: "Front: 최용석, 임대호, 김찬영, 강경오",
+      back: "back: 문승희, 최선우",
+    },
+    {
+      title: "Team TangWay 소개2",
+      front: "Front: 최용석, 임대호, 김찬영, 강경오",
+      back: "back: 문승희, 최선우",
+    },
+    {
+      title: "Team TangWay 소개3",
+      front: "Front: 최용석, 임대호, 김찬영, 강경오",
+      back: "back: 문승희, 최선우",
+    },
+  ],
+  info2: [
+    {
+      title: "Team TangWay 소개1",
+      front: "Front: 최용석, 임대호, 김찬영, 강경오",
+      back: "back: 문승희, 최선우",
+    },
+  ],
+};
